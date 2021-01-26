9f031974408d34648d222d2588eb585fac878378
Afonso Peres <56160428+AfonoDev@users.noreply.github.com>
2021-01-26 00:26:52 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,12 @@ module.exports =  {
     "postgres://postgres:bcd147@localhost:5432/db_guardian", 
       config: { 
         dialect: "postgres", 
+           "dialectOptions": {
+            "ssl": {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
         logging: console.log, 
         define: { 
           timestamp: true, 
