ffed014075aadd6b400c2996a50ea00e7276340f
Afonso Peres <56160428+AfonoDev@users.noreply.github.com>
2021-03-26 19:21:36 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
       url: process.env.DATABASE_URL ||  
-    "postgres://postgres:bcd127@localhost:5432/dbGamesCenter", 
+    "postgres://postgres:bcd127@localhost:5432/db_guardian", 
       config: { 
         dialect: "postgres",
         "dialectOptions": {
